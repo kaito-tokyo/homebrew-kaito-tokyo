d1bef4ccdbf38606af509805d8fbf03321dc41c5
Kaito Udagawa <umireon@kaito.tokyo>
2026-04-10 03:21:32 +0900

    Create kaito-tokyo-numpy@2.4.4.rb
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

---
+++ Formula/kaito-tokyo-numpy@2.4.4.rb
@@ -0,0 +1,38 @@
+class KaitoTokyoNumpyAT244 < Formula
+  desc "Package for scientific computing with Python"
+  homepage "https://www.numpy.org/"
+  url "https://files.pythonhosted.org/packages/d7/9f/b8cef5bffa569759033adda9481211426f12f53299629b410340795c2514/numpy-2.4.4.tar.gz"
+  sha256 "2d390634c5182175533585cc89f3608a4682ccb173cc9bb940b2881c8d6f8fa0"
+  license "BSD-3-Clause"
+
+  keg_only :versioned_formula
+
+  depends_on "gcc" => :build
+  depends_on "kaito-tokyo-python@3.11" => [:build, :test]
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "openblas"
+
+  on_linux do
+    depends_on "patchelf" => :build
+  end
+
+  def install
+    python3 = Formula["kaito-tokyo-python@3.11"].opt_bin/"python3.11"
+    site_packages = Formula["kaito-tokyo-python@3.11"].site_packages
+    system python3, "-m", "pip", "install", "-Csetup-args=-Dblas=openblas",
+                                            "-Csetup-args=-Dlapack=openblas",
+                                            "--target=#{site_packages}"
+                                            *std_pip_args(build_isolation: true), "."
+  end
+
+  test do
+    python3 = Formula["kaito-tokyo-python@3.11"].opt_bin/"python3.11"
+    system python3, "-c", <<~PYTHON
+      import numpy as np
+      t = np.ones((3,3), int)
+      assert t.sum() == 9
+      assert np.dot(t, t).sum() == 27
+    PYTHON
+  end
+end
